bac99659fab5951963fd5d952fae263ff19e7be7
Calandda <A.calandda@gmail.com>
2025-01-06 00:46:29 +0800

    Added main javascript object

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import {home} from './home.js';
 import {about} from './about.js';
 import {menu} from './menu.js'
 
+class mainObject{
+	mainDiv = document.querySelector('#content');
+	constructor(){
+		this.mainDiv.textContent = 'test';
+	};
+}
+
+const mainPage = new mainObject();
 console.log(home.getHomeCheck());
 console.log(about.getAboutCheck());
 console.log(menu.menuCheck);
