8bbfca02d8a9339e386dbf9768ed1f533d404313
Calandda <A.calandda@gmail.com>
2025-01-09 18:38:56 +0800

    Added color change when a nav bar button is pressed

--- src/index.js
+++ src/index.js
@@ -15,14 +15,17 @@ class mainObject{
 			//console.log(template);
 			//const clone = template.cloneNode(true);
 			//this.mainDiv.appendChild(clone);
-			console.log(e.target.className);
-			if(e.target.className === 'buttonHome'){
-				console.log(home.getHomeCheck());
-				home.setHomePage(this.mainDiv, this.shopName);
-			} else if(e.target.className === 'buttonAbout'){
-				console.log(about.getAboutCheck());
-			} else if(e.target.className === 'buttonMenu'){
-				console.log(menu.getMenuCheck());
+			console.log(e.target.tagName);
+			if(e.target.tagName == 'BUTTON'){
+				this.setNavButton(e.target);
+				if(e.target.className === 'buttonHome'){
+					console.log(home.getHomeCheck());
+					home.setHomePage(this.mainDiv, this.shopName);
+				} else if(e.target.className === 'buttonAbout'){
+					console.log(about.getAboutCheck());
+				} else if(e.target.className === 'buttonMenu'){
+					console.log(menu.getMenuCheck());
+				}
 			}
 		});
 	};
@@ -31,6 +34,13 @@ class mainObject{
 			this.mainDiv.removeChild(this.mainDiv.firstChild);
 		}
 	};
+	setNavButton(buttonSelected){
+		const navBar = document.querySelectorAll('.buttonNav');
+		for(let i = 0; i < navBar.length;i++){
+			console.log(navBar[i].classList.remove('colorGray'));
+		};
+		buttonSelected.classList.add('colorGray');
+	}
 }
 
 const mainPage = new mainObject();
